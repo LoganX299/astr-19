ce9bcb8db7f9253c088cb7a394432726d8e22929
Logan Dever <ldever@ucsc.edu>
2024-10-02 14:20:28 -0700

    Add files via upload

---
+++ week-one.py
@@ -0,0 +1,18 @@
+#This program will write
+#Hello World!
+
+#This function tells the
+#computer to print
+#"Hello World!"
+def PrintWeekOne():
+	print("""My name is Logan Dever, my pronouns are He/Him. My favorite Movie is Fast & Furious Tokyo Drift! My Favorite food is a nice bowl of spicy ramen!""")
+
+#This Defines our main()
+#function for our program
+def main():
+	PrintWeekOne()
+
+#when we run the program
+#this executes first
+if __name__=="__main__":
+	main()
